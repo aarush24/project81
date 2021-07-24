6b4e640365a33bef8b61c5ba3c1ad246d39bf857
aarush24 <82024065+aarush24@users.noreply.github.com>
2021-07-24 07:41:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+canvas=document.getElementById("canvas1");
+ctx=canvas.getContext("2d");
+color= "blue";
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=10;
+ctx.arc(100, 200, 80 ,0 , 2*Math.PI); 
+ctx.stroke(); 
+
+
+color= "black";
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=10;
+ctx.arc(200, 200, 80 ,0 , 2*Math.PI); 
+ctx.stroke(); 
+
+color= "red";
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=10;
+ctx.arc(300, 200, 80 ,0 , 2*Math.PI); 
+ctx.stroke(); 
+
+color= "yellow";
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=10;
+ctx.arc(150, 300, 80 ,0 , 2*Math.PI); 
+ctx.stroke(); 
+
+color= "green";
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=10;
+ctx.arc(250, 300, 80 ,0 , 2*Math.PI); 
+ctx.stroke(); 
